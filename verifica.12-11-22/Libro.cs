eb1bab926b931608cdaf781f913b66fbb332665c
Enrico Rotondo <ENRICO.ROTONDO@local.ittcesena.it>
2022-11-12 08:53:26 +0100

    Versione 2: aggiunta della classe libro

--- verifica.12-11-22/Libro.cs
+++ verifica.12-11-22/Libro.cs
@@ -8,5 +8,42 @@ namespace verifica._12_11_22
 {
     internal class Libro
     {
+        private string autore;
+        private string titolo;
+        private int annoPubblicazione;
+        private string editore;
+        private int numeroPagine;
+
+        private string getAutore()
+        {
+            return autore;
+        }
+
+        private string getTitolo()
+        {
+            return titolo;
+        }
+
+        private int getAnnoPubblicazioine()
+        {
+            return annoPubblicazione;
+        }
+
+        private string getEditore()
+        {
+            return editore;
+        }
+
+        private int getNumeroPagine()
+        {
+            return numeroPagine;
+        }
+
+        private void setPagine(int pagine)
+        {
+            this.numeroPagine = pagine;
+        }
+
+
     }
 }
