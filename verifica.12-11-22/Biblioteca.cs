dcda85dac13f701f73c8b9e67765e3d4ae5b9875
Enrico Rotondo <ENRICO.ROTONDO@local.ittcesena.it>
2022-11-12 08:59:18 +0100

    Versione 3: cerazione classe Biblioteca

---
+++ verifica.12-11-22/Biblioteca.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verifica._12_11_22
+{
+    internal class Biblioteca
+    {
+    }
+}
